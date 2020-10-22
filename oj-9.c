0b73517a43944918ea7e019d7278f6eef90e2f80
xsun89 <sunxin89@gmail.com>
2020-10-21 21:04:11 -0700

    initial

--- oj-9.c
+++ oj-9.c
@@ -10,6 +10,23 @@ int factorial(int x)
     }
     return x*factorial(x-1);
 }
+int binarySearch(int (*arr)(int), int n, int x)
+{
+    int head = 1, tail = n, mid;
+    while(head <= tail){
+        mid = (tail + head) >> 1;
+        if(arr(mid) == x){
+            return mid;
+        }
+
+        if(x > arr(mid)){
+            head = mid + 1;
+        }else{
+            tail = mid - 1;
+        }
+    }
+    return -1;
+}
 int main()
 {
 
